bdea645dbeaa009e26dd11b964efba9b51ba8505
qian01_chen <qian01_chen@foxitsoftware.com>
2026-04-20 01:08:03 +0800

    v0.1.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name='dding',
-    version='0.1.11',
+    version='0.1.12',
     keywords='dding',
     description='通知机器人.',
     # version = dding.__version__,
